694dce10d6c9ae277f51a4b2e8892aec175b1f61
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-11-13 18:16:41 +0200

    Initial commit

---
+++ src/main/java/course/springdata/intro/entity/Account.java
@@ -0,0 +1,29 @@
+package course.springdata.intro.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private BigDecimal balance;
+
+    @ManyToOne()
+    @ToString.Exclude
+    private User user;
+
+}
