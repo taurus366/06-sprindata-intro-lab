49bd51d876292d81f87eaabc0c1d163bf6130c9f
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-11-17 16:21:39 +0200

    entity , service,impl

---
+++ src/main/java/course/springdata/intro/exception/NonExistingEntityException.java
@@ -0,0 +1,19 @@
+package course.springdata.intro.exception;
+
+public class NonExistingEntityException extends RuntimeException {
+
+    public NonExistingEntityException() {
+    }
+
+    public NonExistingEntityException(String message) {
+        super(message);
+    }
+
+    public NonExistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NonExistingEntityException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/course/springdata/intro/service/impl/AccountServiceImpl.java
+++ src/main/java/course/springdata/intro/service/impl/AccountServiceImpl.java
@@ -3,14 +3,17 @@ package course.springdata.intro.service.impl;
 import course.springdata.intro.dao.AccountRepository;
 import course.springdata.intro.entity.Account;
 import course.springdata.intro.entity.User;
+import course.springdata.intro.exception.InvalidAccountOperationException;
+import course.springdata.intro.exception.NonExistingEntityException;
 import course.springdata.intro.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
-@Transactional
+@Transactional(propagation = Propagation.REQUIRED)
 @Service
 public class AccountServiceImpl implements AccountService {
     private AccountRepository accountRepo;
@@ -30,16 +33,33 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void withdrawMoney(BigDecimal amount, Long accountId) {
+       Account account = accountRepo.findById(accountId).orElseThrow(() ->
+                new NonExistingEntityException(
+                        String.format("Entity with ID:%s does not exist.",accountId)
+                ));
+       if (account.getBalance().compareTo(amount) < 0){
+           throw new InvalidAccountOperationException(
+                   String.format("Account ID:%s does not have enough balance(%s) is less than required withdraw amount: %s.", accountId,account.getBalance(),amount)
+           );
+       }
+       account.setBalance(account.getBalance().subtract(amount));
+       // commit transaction ! - @Transactional
 
     }
 
     @Override
     public void depositMoney(BigDecimal amount, Long accountId) {
+        Account account = accountRepo.findById(accountId).orElseThrow(() ->
+                new NonExistingEntityException(
+                        String.format("Entity with ID:%s does not exist.",accountId)
+                ));
+        account.setBalance(account.getBalance().add(amount));
 
     }
 
     @Override
     public void transferMoney(BigDecimal amount, Long fromAccountId, Long toAccountId) {
-
-    }
+            depositMoney(amount, toAccountId);
+            withdrawMoney(amount, fromAccountId);
+    } // commit transaction
 }
